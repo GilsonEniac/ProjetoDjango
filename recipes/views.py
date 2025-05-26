0613205b1a7e7acfdc8084f287031157fbd3c62f
Gilson Eniac <gilson.eniac@gmail.com>
2025-05-26 13:38:36 -0300

    ajuste recipes

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request,'global/Home.html')
+    return render(request,'recipes/Home.html')
     
 def sobre(request):
     return HttpResponse('SOBRE 03')
